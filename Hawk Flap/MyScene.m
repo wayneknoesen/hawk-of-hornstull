9432180cf58abb6e2c16109f5634138e4150a68d
Wayne Knoesen <wayne.knoesen@apegroup.com>
2014-05-29 19:44:25 +0200

    Added the collision and it now looks Freeking mad when the bird touches the pipes or the ground.

--- Hawk Flap/MyScene.m
+++ Hawk Flap/MyScene.m
@@ -8,17 +8,22 @@
 
 #import "MyScene.h"
 
-@interface MyScene () {
+@interface MyScene () <SKPhysicsContactDelegate> {
     SKSpriteNode* _bird;
     SKColor* _skyColor;
     SKTexture* _pipeTexture1;
     SKTexture* _pipeTexture2;
     SKAction* _movePipesAndRemove;
+    SKNode* _moving;
     
 }
 @end
 
 @implementation MyScene
+
+static const uint32_t birdCategory = 1 << 0;
+static const uint32_t worldCategory = 1 << 1;
+static const uint32_t pipeCategory = 1 << 2;
 
 static NSInteger const kVerticalPipeGap = 100;
 
@@ -27,9 +32,13 @@ static NSInteger const kVerticalPipeGap = 100;
         /*Setup your size here*/
         
         self.physicsWorld.gravity = CGVectorMake(0.0, -5.0);
+        self.physicsWorld.contactDelegate = self;
         
         _skyColor = [SKColor colorWithRed:113.0/255.0 green:197.0/255.0 blue:207.0/255.0 alpha:1.0];
         [self setBackgroundColor:_skyColor];
+        
+        _moving = [SKNode node];
+        [self addChild:_moving];
         
         SKTexture* birdTexture1 = [SKTexture textureWithImageNamed:@"Bird1"];
         birdTexture1.filteringMode = SKTextureFilteringNearest;
@@ -46,6 +55,9 @@ static NSInteger const kVerticalPipeGap = 100;
         _bird.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:_bird.size.height / 2];
         _bird.physicsBody.dynamic = YES;
         _bird.physicsBody.allowsRotation = NO;
+        _bird.physicsBody.categoryBitMask = birdCategory;
+        _bird.physicsBody.collisionBitMask = worldCategory | pipeCategory;
+        _bird.physicsBody.contactTestBitMask = worldCategory | pipeCategory;
         
         
         [self addChild:_bird];
@@ -73,6 +85,7 @@ static NSInteger const kVerticalPipeGap = 100;
         dummy.position = CGPointMake(0, groundTexture.size.height);
         dummy.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:CGSizeMake(self.frame.size.width, groundTexture.size.height *2)];
         dummy.physicsBody.dynamic = NO;
+        dummy.physicsBody.categoryBitMask = worldCategory;
         [self addChild:dummy];
         
         
@@ -130,12 +143,17 @@ static NSInteger const kVerticalPipeGap = 100;
     pipe1.position = CGPointMake( 0, y );
     pipe1.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:pipe1.size];
     pipe1.physicsBody.dynamic = NO;
+    pipe1.physicsBody.categoryBitMask = pipeCategory;
+    pipe1.physicsBody.contactTestBitMask = birdCategory;
     [pipePair addChild:pipe1];
     
     SKSpriteNode* pipe2 = [SKSpriteNode spriteNodeWithTexture:_pipeTexture2];
     [pipe2 setScale:2];
     pipe2.position = CGPointMake(0, y + pipe1.size.height + kVerticalPipeGap );
+    pipe2.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:pipe2.size];
     pipe2.physicsBody.dynamic = NO;
+    pipe2.physicsBody.categoryBitMask = pipeCategory;
+    pipe2.physicsBody.contactTestBitMask = birdCategory;
     [pipePair addChild:pipe2];
     
     [pipePair runAction:_movePipesAndRemove];
@@ -166,6 +184,18 @@ CGFloat clamp(CGFloat min, CGFloat max, CGFloat value) {
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
     _bird.zRotation = clamp(-1, 0.5, _bird.physicsBody.velocity.dy * (_bird.physicsBody.velocity.dy < 0 ? 0.003 : 0.001 ) );
+}
+
+
+-(void)didBeginContact:(SKPhysicsContact *)contact {
+    //FlashBackground if contact detected
+    [self removeActionForKey:@"flash"];
+    [self runAction:[SKAction sequence:@[[SKAction repeatAction:[SKAction sequence:@[[SKAction runBlock:^{
+        self.backgroundColor = [SKColor redColor];
+    }], [SKAction waitForDuration:0.05], [SKAction runBlock:^{
+        self.backgroundColor = _skyColor;
+    }], [SKAction waitForDuration:0.05]]] count:4]]] withKey:@"flash"];
+    
 }
 
 @end
